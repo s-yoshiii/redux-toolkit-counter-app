4323cba023a0c66d20460f3fd433e44a81963453
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-05-10 00:21:18 +0900

    first commit

---
+++ src/redux/counterSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const consterSlice = createSlice({
+  name: "counter",
+  initialState: {
+    value: 0,
+  },
+  reducers: {
+    increment: (state) => {
+      state.value += 1;
+    },
+    decrement: (state) => {
+      state.value -= 1;
+    },
+  },
+});
+export const { increment, decrement } = consterSlice.actions;
+export default consterSlice.reducer;
