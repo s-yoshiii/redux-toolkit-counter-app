3c984651a9a3114b8752e2edcd043a6cfb4337e4
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-05-10 21:38:07 +0900

    input加算機能実装

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import "./App.css";
+import { decrement, increment, incrementByAmount } from "./redux/counterSlice";
+
+function App() {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+  const [incrementAmount, setIncrementAmount] = useState("2");
+  return (
+    <div className="App">
+      <h1>count:{count}</h1>
+      <input
+        type="number"
+        onChange={(e) => setIncrementAmount(e.target.value)}
+        value={incrementAmount}
+      />
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      <button
+        onClick={() => dispatch(incrementByAmount(Number(incrementAmount)))}
+      >
+        追加
+      </button>
+    </div>
+  );
+}
+
+export default App;
